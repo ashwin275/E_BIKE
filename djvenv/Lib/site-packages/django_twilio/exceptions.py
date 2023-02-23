e89b556a35aea082b56b4f2e97089e3751d42c0f
ashwin275 <110539449+ashwin275@users.noreply.github.com>
2023-02-23 15:23:25 +0530

    Delete djvenv directory

--- djvenv/Lib/site-packages/django_twilio/exceptions.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-class NotDjangoRequestException(Exception):
-    pass
--- djvenv/Lib/site-packages/test_project/test_app/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .client import *
-from .decorators import *
-from .models import *
-from .views import *
-from .request import *
